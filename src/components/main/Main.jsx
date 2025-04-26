ddbbb4d68fea640ad9ab8767304183bf5a5b398e
Zain Usman <zainusman1856@gmail.com.com>
2025-04-26 15:44:00 +0500

    your commit message

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,39 +1,30 @@
-// import React from "react";
-// import "./main.css";
-
-// const Main = ({ diceNumber, rollDice, resetGame, setShowRules, rotate }) => {
-//   return (
-//     <div className="main">
-//       <div className="conntent">
-//         {/* Dice Image - Updates based on diceNumber & rotates on click */}
-//         <img
-//           src={`/src/assets/pic${diceNumber}.png`}
-//           alt="Dice"
-//           onClick={rollDice}
-//           className={rotate ? "rotate" : ""}
-//         />
-
-//         {/* Buttons */}
-//         <button className="btn1" onClick={resetGame}>Reset Score</button>
-//         <button className="btn2" onClick={() => setShowRules(true)}>Show Rules</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Main;
-
-
 import React from "react";
 import "./main.css";
 
+// Import images
+import pic1 from "../../assets/pic1.jpg";
+import pic2 from "../../assets/pic2.jpg";
+import pic3 from "../../assets/pic3.jpg";
+import pic4 from "../../assets/pic4.jpg";
+import pic5 from "../../assets/pic5.jpg";
+import pic6 from "../../assets/pic6.jpg";
+
 const Main = ({ diceNumber, rollDice, resetGame, setShowRules, rotate }) => {
+  // Dynamically select the correct image based on diceNumber
+  let diceImage;
+  if (diceNumber === 1) diceImage = pic1;
+  else if (diceNumber === 2) diceImage = pic2;
+  else if (diceNumber === 3) diceImage = pic3;
+  else if (diceNumber === 4) diceImage = pic4;
+  else if (diceNumber === 5) diceImage = pic5;
+  else if (diceNumber === 6) diceImage = pic6;
+
   return (
     <div className="main">
-      <div className="conntent">
+      <div className="content">
         {/* Dice Image - Updates based on diceNumber & rotates on click */}
         <img
-          src={`/src/assets/pic${diceNumber}.jpg`}
+          src={diceImage} // Use the imported image
           alt="Dice"
           onClick={rollDice}
           className={rotate ? "rotate" : ""}
@@ -48,4 +39,3 @@ const Main = ({ diceNumber, rollDice, resetGame, setShowRules, rotate }) => {
 };
 
 export default Main;
-
